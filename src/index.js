ded1747088a25da85b46583b812bc5916d618996
rushitgondaliya <133621234+rushitgondaliya@users.noreply.github.com>
2023-07-25 09:03:20 +0530

    commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const app = express()
+app.listen(8080,console.log('ok'));
+
+
+
+// var http = require('http');
+// http.createServer(function(req , res){
+//     res.write('rushit');
+//     res.end();
+// }).listen(8080);
